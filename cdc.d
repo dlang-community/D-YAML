cc627074b1962b25d2f06c717656ec47ab2337a0
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-05-19 18:57:31 +0200

    CDC help message edit.

--- cdc.d
+++ cdc.d
@@ -234,7 +234,8 @@ void main(string[] args)
             case "all":      build("debug", "release", "unittest"); break;
             default:
                 writeln("unknown build target: ", target);
-                writeln("available targets: 'debug', 'release', 'profile', 'unittest', 'all'");
+                writeln("available targets: 'debug', 'release', 'profile', "
+                        "'all', 'unittest', 'tar.gz', 'tar.xz', 'zip'");
         }
     }
 
