30d394b8611b43179ef90b9bd20e0eab43f185de
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-07-23 02:54:50 +0200

    Copyright headers in Token, Scanner.

--- source/dyaml/token.d
+++ source/dyaml/token.d
@@ -1,5 +1,5 @@
 
-//          Copyright Ferdinand Majerech 2011.
+//          Copyright Ferdinand Majerech 2011-2014.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
