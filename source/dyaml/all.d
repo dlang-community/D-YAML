72995df8647c5b1300ad01c083fc3504c5f360f9
Cameron Ross <elpenguino@gmail.com>
2018-02-28 20:01:00 -0400

    remove deprecated yaml/dyaml.all modules (#93)
    remove deprecated yaml/dyaml.all modules
    merged-on-behalf-of: BBasile <BBasile@users.noreply.github.com>

--- source/dyaml/all.d
+++
@@ -1,9 +0,0 @@
-
-//          Copyright Ferdinand Majerech 2011.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-
-deprecated("Import dyaml instead") module dyaml.all;
-
-public import dyaml;
