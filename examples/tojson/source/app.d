bb7b87370a5d9891da54296b63c6c3b995d4a937
Cameron Ross <elpenguino@gmail.com>
2018-06-21 00:34:07 -0300

    add tojson subpackage for converting yaml docs to json (#171)
    add tojson subpackage for converting yaml docs to json
    merged-on-behalf-of: BBasile <BBasile@users.noreply.github.com>

---
+++ examples/tojson/source/app.d
@@ -0,0 +1,54 @@
+module dyaml.tojson;
+import std.datetime;
+import std.json;
+import std.stdio;
+import dyaml;
+
+void main()
+{
+    auto doc = Loader.fromFile(stdin).load();
+    auto json = doc.toJSON;
+    writeln(json.toPrettyString);
+}
+
+JSONValue toJSON(Node node)
+{
+    JSONValue output;
+    if (node.isSequence)
+    {
+        output = JSONValue(string[].init);
+        foreach (Node seqNode; node)
+        {
+            output.array ~= seqNode.toJSON();
+        }
+    }
+    else if (node.isMapping)
+    {
+        output = JSONValue(string[string].init);
+        foreach (Node keyNode, Node valueNode; node)
+        {
+            output[keyNode.as!string] = valueNode.toJSON();
+        }
+    }
+    else if (node.isString)
+    {
+        output = node.as!string;
+    }
+    else if (node.isInt)
+    {
+        output = node.as!long;
+    }
+    else if (node.isFloat)
+    {
+        output = node.as!real;
+    }
+    else if (node.isBool)
+    {
+        output = node.as!bool;
+    }
+    else if (node.isTime)
+    {
+        output = node.as!SysTime.toISOExtString();
+    }
+    return output;
+}
