934316e769c471340519c63a5eedad867759ccc1
Cameron Ross <elpenguino@gmail.com>
2019-01-10 20:11:29 -0400

    resurrect resolver example as a pure implicit resolution example

---
+++ examples/resolver/main.d
@@ -0,0 +1,38 @@
+import std.regex;
+import std.stdio;
+import dyaml;
+
+int main(string[] args)
+{
+    string path = "input.yaml";
+    if (args.length > 1)
+    {
+        path = args[1];
+    }
+
+    try
+    {
+        auto resolver = new Resolver;
+        resolver.addImplicitResolver("!color", regex("[0-9a-fA-F]{6}"),
+            "0123456789abcdefABCDEF");
+
+        auto loader = Loader.fromFile("input.yaml");
+        loader.resolver = resolver;
+
+        auto root = loader.load();
+
+        if(root["scalar-red"].tag == "!color" &&
+            root["scalar-orange"].tag == "!color")
+        {
+            writeln("SUCCESS");
+            return 0;
+        }
+    }
+    catch(YAMLException e)
+    {
+        writeln(e.msg);
+    }
+
+    writeln("FAILURE");
+    return 1;
+}
