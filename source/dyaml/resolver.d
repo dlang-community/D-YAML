aa9b4c8c2b62eca0389adc483c3ef9bd783c319a
Cameron Ross <elpenguino@gmail.com>
2019-01-24 02:01:40 -0400

    fix a Resolver test

--- source/dyaml/resolver.d
+++ source/dyaml/resolver.d
@@ -95,7 +95,7 @@ final class Resolver
             }
         }
         /// Resolve scalars starting with 'A' to !_tag
-        unittest
+        @safe unittest
         {
             import std.file : write;
             import std.regex : regex;
@@ -109,11 +109,8 @@ final class Resolver
             resolver.addImplicitResolver("!tag", regex("A.*"), "A");
             loader.resolver = resolver;
 
-            //Note that we have no constructor from tag "!tag", so we can't
-            //actually load anything that resolves to this tag.
-            //See Constructor API documentation and tutorial for more information.
-
-            //auto node = loader.load();
+            auto node = loader.load();
+            assert(node.tag == "!tag");
         }
 
     package:
